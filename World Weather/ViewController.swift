f2ad8175635d219165af0dc91ff8adda5c9cd422
ravisharmaa <bastola.ravi@gmail.com>
2017-12-09 17:25:30 +0545

    Minor Changes and a New Branch

--- World Weather/ViewController.swift
+++ World Weather/ViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*
+     * Custom View Hierarchy
+     *
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        CG Rect Instance
+        /* CG Rect Instance */
         
         let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
         let firstView  = UIView(frame: firstFrame)
         firstView.backgroundColor = UIColor.blue
         view.addSubview(firstView)
+        
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 40)
+        let secondView  = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        firstView.addSubview(secondView)
+        
+        let thirdFrame = CGRect(x: 10, y: 10, width: 10, height: 20)
+        let thirdView = UIView(frame: thirdFrame)
+        thirdView.backgroundColor = UIColor.red
+        secondView.addSubview(thirdView)
     }
+ 
+     */
+    
 
 
 }
